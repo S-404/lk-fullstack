55cab8deee0c9addf5123e74a0b081426f695fb7
S-404 <sudarev.andrey.i@gmail.com>
2022-04-07 16:09:56 +0300

    [server] add express-validator to auth-routes

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,10 +1,15 @@
-
 const userService = require('../service/user-service')
+const {validationResult} = require('express-validator')
+const ApiError = require('../exceptions/api-error')
 
 class AuthController {
 
     async registration(req, res, next) {
         try {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return next(ApiError.BadRequest('Validation error',errors.array()))
+            }
             const {username, password} = req.body
             const userData = await userService.registration(username, password)
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
