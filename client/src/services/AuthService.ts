e43fc27cdfba2bf0b7ff9c2f72e9f8557dc94d95
S-404 <sudarev.andrey.i@gmail.com>
2022-04-08 13:25:53 +0300

    [client] refact code

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,6 +1,6 @@
 import {_api} from '../api'
 import {AxiosResponse} from 'axios'
-import {AuthResponse} from "./types/AuthResponse";
+import {AuthResponse} from "../types/services/AuthResponse";
 
 export default class AuthService{
 
