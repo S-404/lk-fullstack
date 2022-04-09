765bc1b33e2068314c2805af86637626e8cf5ea7
S-404 <sudarev.andrey.i@gmail.com>
2022-04-09 17:13:36 +0300

    [client] define error messages in catch blocks

--- client/src/store/action-creators/login.ts
+++ client/src/store/action-creators/login.ts
@@ -1,6 +1,7 @@
 import {Dispatch} from "redux"
 import {AuthAction, AuthActionTypes} from "../../types/store/authTypes"
 import AuthService from "../../services/authService"
+import axios from "axios"
 
 export const login = (username: string, password: string) => {
     return async (dispatch: Dispatch<AuthAction>) => {
@@ -11,9 +12,14 @@ export const login = (username: string, password: string) => {
             dispatch({type: AuthActionTypes.FETCH_USER_SUCCESS, value: response.data.user})
             dispatch({type: AuthActionTypes.SET_AUTH, value: true})
         } catch (e) {
+            let errMsg
+            if (axios.isAxiosError(e)) {
+                errMsg = e?.response?.data?.message
+            }
+            let message = errMsg ? errMsg : "connection failed"
             dispatch({
                 type: AuthActionTypes.FETCH_USER_ERROR,
-                value: "password verification error: connection failed"
+                value: message
             })
         }
     }
--- client/src/store/action-creators/registration.ts
+++ client/src/store/action-creators/registration.ts
@@ -1,18 +1,24 @@
 import {Dispatch} from "redux"
 import {AuthAction, AuthActionTypes} from "../../types/store/authTypes"
 import AuthService from "../../services/authService"
+import axios from "axios"
 
 export const registration = (username: string, password: string) => {
     return async (dispatch: Dispatch<AuthAction>) => {
         try {
             dispatch({type: AuthActionTypes.FETCH_USER})
             const response = await AuthService.registration(username, password)
-            localStorage.setItem('token', response.data.accessToken)
+            localStorage.setItem("token", response.data.accessToken)
             dispatch({type: AuthActionTypes.FETCH_USER_SUCCESS, value: response.data.user})
         } catch (e) {
+            let errMsg
+            if (axios.isAxiosError(e)) {
+                errMsg = e?.response?.data?.message
+            }
+            let message = errMsg ? errMsg : "connection failed"
             dispatch({
                 type: AuthActionTypes.FETCH_USER_ERROR,
-                value: "password verification error: connection failed"
+                value: message
             })
         }
     }
