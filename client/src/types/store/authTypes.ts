8ac21ce00a772ffd1cecd422914e9f7d87b80d5a
S-404 <sudarev.andrey.i@gmail.com>
2022-04-09 15:05:58 +0300

    [client] upd store

--- client/src/types/store/authTypes.ts
+++ client/src/types/store/authTypes.ts
@@ -1,9 +1,17 @@
+import {IUser} from "../services/AuthResponse"
+
 export interface AuthState {
     isAuth: boolean;
+    user: IUser;
+    loading: boolean;
+    error: null | string;
 }
 
 export enum AuthActionTypes {
     SET_AUTH = "SET_AUTH",
+    FETCH_USER = "FETCH_USER",
+    FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS",
+    FETCH_USER_ERROR = "FETCH_USER_ERROR",
 }
 
 interface SetAuthAction {
@@ -11,4 +19,24 @@ interface SetAuthAction {
     value: boolean;
 }
 
-export type  AuthAction = SetAuthAction
+
+interface FetchUserAction {
+    type: AuthActionTypes.FETCH_USER;
+}
+
+interface FetchUserSuccessAction {
+    type: AuthActionTypes.FETCH_USER_SUCCESS;
+    value: IUser;
+}
+
+interface FetchUserErrorAction {
+    type: AuthActionTypes.FETCH_USER_ERROR;
+    value: string;
+}
+
+
+export type  AuthAction =
+    SetAuthAction
+    | FetchUserAction
+    | FetchUserSuccessAction
+    | FetchUserErrorAction
