ecd1475ef82b22217bb37afd96e6322aacaa398f
S-404 <sudarev.andrey.i@gmail.com>
2022-04-10 14:39:25 +0300

    [server] upd userDto

--- server/dtos/user-dto.js
+++ server/dtos/user-dto.js
@@ -1,9 +1,12 @@
-module.exports = class UserDto{
-    id;
-    userId;
+module.exports = class UserDto {
+    id
+    userId
+    username
+
     constructor(model) {
         this.id = model.id
         this.userId = model.id
+        this.username = model.username
     }
 }
 
