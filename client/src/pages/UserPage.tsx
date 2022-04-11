3b8c84f524caa189627e745f382980a682602aa1
S-404 <sudarev.andrey.i@gmail.com>
2022-04-11 17:08:50 +0300

    [client] upd UserPage

--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -1,10 +1,14 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 const UserPage = () => {
 
     return (
-        <div>
-            <h3>Personal Page</h3>
+        <div className='d-flex justify-content-center align-items-center h-75'>
+            <div>
+                <p>Personal Page is empty</p>
+                <p>You can use your <Link to={'/contacts'}>Contact List</Link></p>
+            </div>
         </div>
     )
 }
